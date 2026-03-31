1c602b59f435f7426ff200100c1f0ca81983ef12
Ataqah Ramadhan Widiyana Putra <ataqah.ramadhan@gmail.com>
2026-03-31 16:08:30 +0700

    Latihan

--- Latihan/src/Main.java
+++ Latihan/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         MesinKopi mesinlobby = new MesinKopi();
+
+        mesinlobby.isiUlangBahan(200, 1000, 500);
+
+        System.out.println("Apakah Cappuccino tersedia? " + mesinlobby.cekKetersediaanCappuccino());
+
     }
 }
--- Latihan/src/MesinKopi.java
+++ Latihan/src/MesinKopi.java
@@ -1,5 +1,28 @@
 public class MesinKopi {
+
     double bijiKopi;
     double air;
     double susu;
+
+    public void isiUlangBahan(double bijiKopi, double air, double susu) {
+        this.bijiKopi += bijiKopi;
+        this.air += air;
+        this.susu += susu;
+
+        System.out.println("Bahan Baku Berhasil diisi Ulang.");
+    }
+
+    boolean cekKetersediaanCappuccino() {
+        if (bijiKopi >= 15 && air >= 50 && susu >= 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    MesinKopi() {
+        this.bijiKopi = 0;
+        this.air = 0;
+        this.susu = 0;
+    }
 }
